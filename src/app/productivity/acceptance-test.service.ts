bd2eba4474d7ceedb0b6edee154677605c40037a
MaximSemenov <maxim.n.semenov@gmail.com>
2018-01-15 12:32:47 -0500

    [MaximSemenov/algorithmus] Page build failure is fixed

--- src/app/productivity/acceptance-test.service.ts
+++ src/app/productivity/acceptance-test.service.ts
@@ -28,8 +28,8 @@ export class AcceptanceTestService {
 
   startTesting(id): Observable<testResults[]> {
 
-        // return this.http.get<testResults[]>(this.baseUrl, { params: { problemId: id } });
-      return this.http.get<testResults[]>('https://stark-tundra-14847.herokuapp.com/', { params: { problemId: id } });
+         return this.http.get<testResults[]>(this.baseUrl, { params: { problemId: id } });
+      // return this.http.get<testResults[]>('https://stark-tundra-14847.herokuapp.com/', { params: { problemId: id } });
     // return this.http.get<testResults[]>('http://localhost:5000/', { params: { problemId: id } });
    // linode
     // return this.http.get<testResults[]>('http://173.255.247.60:5000/', { params: { problemId: id } });
